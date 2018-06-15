443003a71bef938006dc0aab2f0d0044e84ff9f0
leandra <leandra.reid@gmail.com>
2018-06-15 15:54:12 -0400

    finished app

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var request = require('request');
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
@@ -15,7 +16,6 @@ switch (command) {
         myTweets();
         break;
 
-// how to integrate if no song input??
         case `spotify-this-song`:
         spotifyThisSong(userInput);
         break;
@@ -25,11 +25,7 @@ switch (command) {
         break;
 
         case `do-what-it-says`:
-        //read file first
-        // form of string --> convert to array
-        // split + grab name of song
-        // pass name of song to song function
-        //add to .txt file
+        doWhatItSays();
         break;
 
     default:
@@ -46,35 +42,41 @@ function myTweets() {
             for (var i = 0; i <= 19; i++) {
                 console.log("Tweet number " + [i] + " : " + tweets[i].text);
                 console.log("This was tweeted on: " + tweets[i].created_at);
+                console.log("===============================");
             };
         };
     });
 };
 
 
-
-//spotify sample function
 function spotifyThisSong(userInput){
 
     if (!userInput){
         noSongInput();
     } else {
-    //if else statement for missing input (give song the sign by ace of base)
-    spotify
-        .search({
-            type: 'track',
-            query: userInput
-        })
-        .then(function (response) {
-            var songs = response.tracks.items;
-            console.log("You chose the song " + songs[0].name);
-            console.log("This song is by artist " + songs[0].artists[0].name);
-            console.log("This song is from the album " + songs[0].album.name);
-            console.log("You can play the song here:" + songs[0].external_urls.spotify);
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
+        spotify
+            .search({
+                type: 'track',
+                query: userInput
+            })
+            .then(function (response) {
+                var songs = response.tracks.items;
+                console.log("===============================");
+                console.log("You chose the song " + songs[0].name);
+                console.log("This song is by artist " + songs[0].artists[0].name);
+                console.log("This song is from the album " + songs[0].album.name);
+                console.log("You can play the song here:" + songs[0].external_urls.spotify);
+                console.log("===============================");
+
+                fs.appendFile("log.txt", ", Song name : " + userInput, function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                });
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
     };
 };
 
@@ -87,10 +89,12 @@ function noSongInput(){
         })
         .then(function (response) {
             var songs = response.tracks.items;
+            console.log("===============================");
             console.log("You didn't choose the song but I'm show it to you anyways : " + songs[0].name);
             console.log("This song is by artist " + songs[0].artists[0].name);
             console.log("This song is from the album " + songs[0].album.name);
             console.log("You can play the song here: " + songs[0].external_urls.spotify);
+            console.log("===============================");
         })
         .catch(function (err) {
             console.log(err);
@@ -98,13 +102,13 @@ function noSongInput(){
 };
 
 
-
 function movieLookup(userInput) {
-    //request sample function
-
+    if (!userInput) {
+        noMovieInput();
+    } else {
     request('http://www.omdbapi.com/?t=' + userInput + '&apikey=trilogy', function (error, response, body) {
         var movie = JSON.parse(body);
-        console.log(movie);
+        console.log("===============================");
         console.log("You have chosen the movie : " + movie.Title);
         console.log("It was released on : " + movie.Released);
         console.log("IMDB gives it a rating of : " + movie.Ratings[0].Value);
@@ -113,8 +117,41 @@ function movieLookup(userInput) {
         console.log("It was made in : " + movie.Language);
         console.log("A brief plot summary : " + movie.Plot);
         console.log("It stars : " + movie.Actors);
+        console.log("===============================");
+        fs.appendFile("log.txt", ", Movie name : " + userInput, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    });
+    };
+};
+
+function noMovieInput() {
+    request('http://www.omdbapi.com/?t=' + 'mr-nobody' + '&apikey=trilogy', function (error, response, body) {
+        var movie = JSON.parse(body);
+        console.log("===============================");
+        console.log("You have not chosen the movie, so I have chosen for you (bye bye autonomy): " + movie.Title);
+        console.log("It was released on : " + movie.Released);
+        console.log("IMDB gives it a rating of : " + movie.Ratings[0].Value);
+        console.log("Rotten Tomatoes gives it a rating of : " + movie.Ratings[1].Value);
+        console.log("It was produced in : " + movie.Country);
+        console.log("It was made in : " + movie.Language);
+        console.log("A brief plot summary : " + movie.Plot);
+        console.log("It stars : " + movie.Actors);
+        console.log("===============================");
     });
 };
 
-
-
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            logOutput.error(err);
+        } else {
+            var randomArray = [];
+            var songName = data.split(",")
+            randomArray.push(songName[1]);
+            spotifyThisSong(randomArray);
+        };
+    });
+};
